7fefabdf3084d5ee5c4e7261d8edf025517a170b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-30 17:24:34 +0000

    is loading

--- lib/features/home/homePage.dart
+++ lib/features/home/homePage.dart
@@ -9,19 +9,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isLoading = false;
 
+  void startDownload() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+            child: ElevatedButton(
+              onPressed: (){},
+              child: Text("Download"),
             ),
-          ],
         ),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     const Text(
+        //       'You have pushed the button this many times:',
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
