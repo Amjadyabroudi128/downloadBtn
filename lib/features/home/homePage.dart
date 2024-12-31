fc46a77e2a2663bf17b035e4499f267b1bbb0fc1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-31 14:23:57 +0000

    container width

--- lib/features/home/homePage.dart
+++ lib/features/home/homePage.dart
@@ -37,41 +37,46 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: (isDownloading || isDownloaded) ? null : startDownload,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-            textStyle: const TextStyle(fontSize: 16),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              if (isDownloading)
-                const Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: LinearProgressIndicator(
-                    minHeight: 4,
-                    color: Colors.white,
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(isDownloaded ? Icons.check : Icons.download),
-                  const SizedBox(width: 8),
-                  Text(
-                    isDownloading
-                        ? "Downloading..."
-                        : (isDownloaded ? "Downloaded" : "Download"),
-                  ),
-                ],
-              ),
-            ],
+        child: Container(
+          width: MediaQuery.of(context).size.width / 1.49,
+          // height: 40,
+          child: ElevatedButton(
+            onPressed: (isDownloading || isDownloaded) ? null : startDownload,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+              textStyle: const TextStyle(fontSize: 16),
+            ),
+            child: Text("Download"),
+            // child: Stack(
+            //   alignment: Alignment.center,
+            //   children: [
+            //     if (isDownloading)
+            //       const Positioned(
+            //         bottom: 0,
+            //         left: 0,
+            //         right: 0,
+            //         child: LinearProgressIndicator(
+            //           minHeight: 4,
+            //           color: Colors.white,
+            //           backgroundColor: Colors.grey,
+            //         ),
+            //       ),
+            //     Row(
+            //       mainAxisSize: MainAxisSize.min,
+            //       children: [
+            //         Icon(isDownloaded ? Icons.check : Icons.download),
+            //         const SizedBox(width: 8),
+            //         Text(
+            //           isDownloading
+            //               ? "Downloading..."
+            //               : (isDownloaded ? "Downloaded" : "Download"),
+            //         ),
+            //       ],
+            //     ),
+            //   ],
+            // ),
           ),
         ),
       ),
