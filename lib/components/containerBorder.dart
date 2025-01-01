4c4ff1f4f8ed11259b0c17840bf6dae547133cea
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-01 15:55:02 +0000

    color class

--- lib/components/containerBorder.dart
+++ lib/components/containerBorder.dart
@@ -1,10 +1,11 @@
 
- import 'package:flutter/material.dart';
+ import 'package:download_button/components/colors.dart';
+import 'package:flutter/material.dart';
 
 class containerBorder {
 
   static BorderSide btm = BorderSide(
-        color: Colors.grey.shade600,
+        color: myColor,
         width: 4,
   );
   static BorderSide right = BorderSide(
