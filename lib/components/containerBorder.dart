cff172cb88994580a83d52a952ef1c29bb73502c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-01 14:52:14 +0000

    btm border side

---
+++ lib/components/containerBorder.dart
@@ -0,0 +1,15 @@
+
+ import 'package:flutter/material.dart';
+
+class containerBorder {
+  static BorderSide btm = BorderSide(
+        color: Colors.grey.shade600,
+        width: 4,
+  );
+  static Border right = Border(
+    right: BorderSide(
+        color: Colors.grey.shade600,
+        width: 3
+    ),
+  );
+ }
