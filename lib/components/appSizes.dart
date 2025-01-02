0ad1f376fd58d3c60f70dd88c21857ab19934a6a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-02 18:26:38 +0000

    container size class

---
+++ lib/components/appSizes.dart
@@ -0,0 +1,11 @@
+
+ import 'package:flutter/material.dart';
+
+ class myContainerSizes {
+   static cWidth(BuildContext context) {
+     return MediaQuery.of(context).size.width / 1.60;
+   }
+   static cHeight(BuildContext context) {
+     return MediaQuery.of(context).size.height / 18.5;
+   }
+ }
