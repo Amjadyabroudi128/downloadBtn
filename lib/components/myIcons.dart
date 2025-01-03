f9004ec0b10d8ed88cdadeb9c91e499dce1b4ec7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-03 18:26:01 +0000

    download icon

---
+++ lib/components/myIcons.dart
@@ -0,0 +1,7 @@
+
+ import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class myIcons {
+  static Icon download = Icon(Icons.download, color: myColor.iconClr, size: 26,);
+ }
