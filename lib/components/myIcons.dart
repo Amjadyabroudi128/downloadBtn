5b47a9ed44c6019b3da4c6c268a4cae72a13a2d3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-03 18:32:47 +0000

    text color

--- lib/components/myIcons.dart
+++ lib/components/myIcons.dart
@@ -2,6 +2,8 @@
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
+
 class myIcons {
   static Icon download = Icon(Icons.download, color: myColor.iconClr, size: 26,);
  }
