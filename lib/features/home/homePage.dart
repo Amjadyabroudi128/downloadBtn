5a634b9b7ee9768e2d8df7bbb6bffbf279b9adae
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-06 14:19:34 +0000

    text widget

--- lib/features/home/homePage.dart
+++ lib/features/home/homePage.dart
@@ -4,6 +4,7 @@ import 'package:download_button/components/colors.dart';
 import 'package:download_button/components/containerBorder.dart';
 import 'package:download_button/components/myIcons.dart';
 import 'package:download_button/components/textStyles.dart';
+import 'package:download_button/features/home/widgets/textWidget.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
