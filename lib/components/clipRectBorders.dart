1127a028304f7585ea33c8789208eef11c5cd437
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-11 16:17:56 +0000

    clipRect Borders

---
+++ lib/components/clipRectBorders.dart
@@ -0,0 +1,9 @@
+
+ import 'package:flutter/cupertino.dart';
+
+class cBorder {
+  static BorderRadiusGeometry borders = BorderRadius.only (
+    bottomLeft: Radius.circular(4),
+    bottomRight: Radius.circular(4),
+  );
+ }
