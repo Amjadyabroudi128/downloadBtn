4ddffab05002c8376b35ed49204d9216e1008372
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-12 15:43:58 +0000

    positioned

--- lib/features/home/widgets/Positioned.dart
+++ lib/features/home/widgets/Positioned.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class positionedW extends StatelessWidget {
   final Widget? myChild;
   const positionedW({super.key, this.myChild});
-
+  static double pS = 0;
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: 0,
-      left: 0,
-      right: 0,
+      bottom: pS,
+      left: pS,
+      right: pS,
       child: myChild!,
     );
   }
