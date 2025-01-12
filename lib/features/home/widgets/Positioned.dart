6078b9320fc2a794e6baac2d14959d226e1bf29b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-12 15:46:46 +0000

    size helper

--- lib/features/home/widgets/Positioned.dart
+++ lib/features/home/widgets/Positioned.dart
@@ -1,15 +1,16 @@
+import 'package:download_button/components/appSizes.dart';
 import 'package:flutter/material.dart';
 
 class positionedW extends StatelessWidget {
   final Widget? myChild;
   const positionedW({super.key, this.myChild});
-  static double pS = 0;
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: pS,
-      left: pS,
-      right: pS,
+      bottom: sizeHelper.pS,
+      left: sizeHelper.pS,
+      right: sizeHelper.pS,
       child: myChild!,
     );
   }
