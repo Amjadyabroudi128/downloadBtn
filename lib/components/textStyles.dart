cd9e7678a9cf413621c5062bf5136c25987a4ce0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-18 13:59:39 +0000

    snackbar text

--- lib/components/textStyles.dart
+++ lib/components/textStyles.dart
@@ -4,4 +4,5 @@ import 'colors.dart';
 
 class Textstyles {
   static TextStyle dText = TextStyle(fontSize: 18, color: myColor.txtClr);
+  static TextStyle barText = TextStyle(fontSize: 17, color: Colors.white);
  }
