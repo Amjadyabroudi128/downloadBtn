e54e90aaad69e9ccaafbdefe4843812c7ea42658
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-19 14:59:10 +0000

    font size

--- lib/components/textStyles.dart
+++ lib/components/textStyles.dart
@@ -1,8 +1,9 @@
+import 'package:download_button/components/appSizes.dart';
 import 'package:flutter/material.dart';
 
 import 'colors.dart';
 
 class Textstyles {
-  static TextStyle dText = TextStyle(fontSize: 18, color: myColor.txtClr);
+  static TextStyle dText = TextStyle(fontSize: sizeHelper.F18, color: myColor.txtClr);
   static TextStyle barText = TextStyle(fontSize: 17, color: myColor.barTextClr);
  }
