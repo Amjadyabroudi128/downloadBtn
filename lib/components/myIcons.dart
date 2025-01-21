c7d900308798cc978ed5513d43a52eed1e5a2776
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-21 15:08:46 +0000

    close icon

--- lib/components/myIcons.dart
+++ lib/components/myIcons.dart
@@ -2,8 +2,6 @@
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'colors.dart';
-
 class myIcons {
   static Icon download = Icon(Icons.download,);
   static Icon check = Icon(Icons.check,);
