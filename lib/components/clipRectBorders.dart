0e6ed5bd702095760a50e27bf02bd56c69d372cb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-21 15:07:18 +0000

    radius

--- lib/components/clipRectBorders.dart
+++ lib/components/clipRectBorders.dart
@@ -1,11 +1,11 @@
 
  import 'package:download_button/components/appSizes.dart';
+import 'package:download_button/components/containerBorder.dart';
 import 'package:flutter/cupertino.dart';
 
 class cBorder {
-  static Radius circular = Radius.circular(sizeHelper.circular4);
   static BorderRadiusGeometry borders = BorderRadius.only (
-    bottomLeft: circular,
-    bottomRight: circular,
+    bottomLeft: containerBorder.circularindicator,
+    bottomRight: containerBorder.circularindicator,
   );
  }
