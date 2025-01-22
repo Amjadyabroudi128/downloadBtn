fc8d0f575c208a42bfc3daea5476a55aa9ecd17d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-22 18:34:03 +0000

    making my own close icon

--- lib/components/snackbar.dart
+++ lib/components/snackbar.dart
@@ -2,26 +2,29 @@ import 'package:download_button/components/Duration.dart';
 import 'package:download_button/components/colors.dart';
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context, String message, {Duration? duration}) {
+void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Container(
-        height: 18,
+        height: 18, // Adjust the height as needed
+        alignment: Alignment.center,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center, // Ensures everything aligns
           children: [
-            Expanded(child: Text(message)),
+            Expanded(
+              child: Text(message),
+            ),
             IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.remove, size: 16,),
-            )
+              icon: Icon(Icons.remove, color: Colors.white, size: 16), // Smaller icon
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              },
+              padding: EdgeInsets.zero, // Remove extra padding
+            ),
           ],
         ),
-        padding: EdgeInsets.zero,
       ),
-      duration: duration ?? sec2(),
-      dismissDirection: DismissDirection.down, // Custom direction for dismissing (sliding down).
     ),
   );
 }
