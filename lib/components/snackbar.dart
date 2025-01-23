d04a194183fef35c4354a7298c2dd696a526b249
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-23 14:35:22 +0000

    close icon

--- lib/components/snackbar.dart
+++ lib/components/snackbar.dart
@@ -1,5 +1,6 @@
 import 'package:download_button/components/Duration.dart';
 import 'package:download_button/components/colors.dart';
+import 'package:download_button/components/myIcons.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String message) {
@@ -16,7 +17,7 @@ void showSnackBar(BuildContext context, String message) {
               child: Text(message),
             ),
             IconButton(
-              icon: Icon(Icons.remove,size: 16),
+              icon: myIcons.Close,
               onPressed: () {
                 ScaffoldMessenger.of(context).hideCurrentSnackBar();
               },
