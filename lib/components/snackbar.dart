213a4c94fdb2a48e862f33d4d9ad8a6eb781242b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-25 14:31:16 +0000

    container height

--- lib/components/snackbar.dart
+++ lib/components/snackbar.dart
@@ -1,3 +1,4 @@
+import 'package:download_button/components/appSizes.dart';
 import 'package:flutter/material.dart';
 
 import '../features/home/widgets/iconButton.dart';
@@ -6,7 +7,7 @@ void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Container(
-        height: 18,
+        height: sizeHelper.height18,
         alignment: Alignment.center,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
