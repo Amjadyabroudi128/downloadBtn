56cb23acfee7cdd0e0830414c29a29d9c1de5fa9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-26 14:31:57 +0000

    added padding

--- lib/components/iconButton.dart
+++ lib/components/iconButton.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class closeBtn extends StatelessWidget {
   final Widget kIcon;
   final VoidCallback? press;
-  const closeBtn({super.key, required this.kIcon, this.press});
+  final EdgeInsetsGeometry? myPadding;
+  const closeBtn({super.key, required this.kIcon, this.press, this.myPadding});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: press,
       icon: kIcon,
+      padding: myPadding,
     );
   }
 }
