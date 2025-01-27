6f9a9d8768fb2315e84e04fa94e748f5dffcdca7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-27 15:58:59 +0000

    close icon size

--- lib/components/myIcons.dart
+++ lib/components/myIcons.dart
@@ -1,10 +1,11 @@
 
- import 'package:download_button/components/colors.dart';
+ import 'package:download_button/components/appSizes.dart';
+import 'package:download_button/components/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class myIcons {
   static Icon download = Icon(Icons.download,);
   static Icon check = Icon(Icons.check,);
-  static Icon Close = Icon(Icons.remove, color: myColor.closeIcon,size: 16,);
+  static Icon Close = Icon(Icons.remove, color: myColor.closeIcon,size: sizeHelper.iconSize,);
  }
